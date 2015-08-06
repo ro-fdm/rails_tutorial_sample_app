96db31b02ba88c57c5e8331b82c86843374d8a1a
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-08-06 22:08:36 +0200

    Add contact static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,5 +24,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@title}"
+  end
 
 end
