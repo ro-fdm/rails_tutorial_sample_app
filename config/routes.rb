082bca2054064cc985962d65538fffc95c290b2f
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-09-03 21:09:36 +0200

    las rutas nuevas

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,12 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup'  => 'users#new'
+
+  get 'login'   => 'session#new'
+
+  get 'login'   => 'session#create'
+
+  get 'login'   => 'session#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
