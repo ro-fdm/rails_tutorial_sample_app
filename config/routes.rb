3a771abe677c2db33a495e9bed3e7222e5ecf05d
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-09-08 22:35:38 +0200

    los verbos http son importantes para rails

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,15 @@ Rails.application.routes.draw do
   get 'user/new'
 
   get 'home'    => 'static_pages#home'
-
   get 'help'    => 'static_pages#help'
-
   get 'about'   => 'static_pages#about'
-
   get 'contact' => 'static_pages#contact'
 
   get 'signup'  => 'users#new'
 
   get 'login'   => 'sessions#new'
-
-  get 'login'   => 'sessions#create'
-
-  get 'logout'  => 'sessions#destroy'
+  post 'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
