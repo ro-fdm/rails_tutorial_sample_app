6956ba2fb4f3541b1eee384f834c8ba69a551910
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-09-12 14:03:04 +0200

    añadida funcion autentificacion en el modelo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accesor :remember_token
+  attr_accessor :remember_token
   before_save {self.email = email.downcase}
   validates :name,  presence: true, length: { maximum: 50 }
 
@@ -23,13 +23,13 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-  # Returns a random token
-  def User.new_token 
-    SecureRandom.urlsafe_base64
-  end
-
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if given token matches the digest
+  def authenticated(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
