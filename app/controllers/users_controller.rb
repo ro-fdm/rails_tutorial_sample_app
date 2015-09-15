468e620ff5fe4e9532f594a661f6d74e9a71ac1f
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-09-15 22:44:39 +0200

    9.1.4 con fallos en el test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.new(user_params)
     if @user.update_attributes(user_params)
-      # Handle a successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
