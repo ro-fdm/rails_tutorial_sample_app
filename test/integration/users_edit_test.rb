1fccba178129c9caadb7234c2a01886067189529
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-09-18 22:45:27 +0200

    Arreglado bug en test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -24,11 +24,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name:  name,
-                                    email: email,
-                                    password:              "",
-                                    password_confirmation: "" }
+                                    email: email}
     assert_not flash.empty?
-    assert_redirected_to @user
+    assert_redirected_to @user, response.body
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
