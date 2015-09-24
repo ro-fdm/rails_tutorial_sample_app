58215bbf9fc51e04e94d8fbbedb9a8b7afa2f61e
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-09-24 22:12:02 +0200

    Añadir activacion de usuario, cosas basicas

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :users
+
+  resources :account_activations, only: [:edit]
   # Example resource route with options:
   #   resources :products do
   #     member do
