128914ad086dd49e8ba4adc529a2a26cc55756a0
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-10-06 22:36:03 +0200

    Solo accedemos a los microposts si estamos logueados

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,8 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  
+  resources :microposts,          only: [:create, :destroy]
   # Example resource route with options:
   #   resources :products do
   #     member do
