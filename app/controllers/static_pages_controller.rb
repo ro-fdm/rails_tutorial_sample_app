686528f5336e709cb7cb975b2a6421a0461f7455
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2015-10-07 21:47:35 +0200

    Bug micropost por microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.micropost.build if logged_in?
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
