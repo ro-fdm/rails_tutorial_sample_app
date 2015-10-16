04150da2ff58f02f2e71e7949f9948f602a3f9f8
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-10-16 22:34:46 +0200

    testeando el feed

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -25,12 +25,12 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == admin
+      unless user == @admin
          assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
-    assert_diference 'User.count', -1 do
-      delete users_path(@non_admin)
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
     end
   end
 
